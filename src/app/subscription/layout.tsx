8a23ee9b5965873b100fc811868d47bb90d558fd
SHARATHKM2004 <sharathkmsharu2004@gmail.com>
2026-05-19 22:39:08 +0530

    Add footer to subscription page

---
+++ src/app/subscription/layout.tsx
@@ -0,0 +1,19 @@
+import { draftMode } from "next/headers";
+import { SiteFooter } from "@/components/site/site-footer";
+import { getSiteFooterContent } from "@/lib/cms";
+
+export default async function SubscriptionLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const draft = await draftMode();
+  const footerContent = await getSiteFooterContent("en", draft.isEnabled);
+
+  return (
+    <div className="site-shell flex min-h-screen flex-col">
+      <div className="flex-1">{children}</div>
+      <SiteFooter locale="en" content={footerContent} />
+    </div>
+  );
+}
