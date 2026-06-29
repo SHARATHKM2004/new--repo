523d821e7c4904c186059d7c68bbf5a648944d9a
Vaishali Singh <Vaishali.Singh@wipfli.com>
2026-06-29 12:27:18 +0530

    fix: set enable_iframe=false to render registration form directly without iframe

--- src/components/cms/bigmarker-registration-widget.tsx
+++ src/components/cms/bigmarker-registration-widget.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 
 export function BigMarkerRegistrationWidget({ conferenceId }: { conferenceId: string }) {
   const containerId = `bigmarker-conference-widget-container${conferenceId}`;
-  const scriptSrc = `https://web.bigmarker.com/widget/register_widget.js?club=wipfli&conference=${conferenceId}&widget_type=form_register&series_register=&upcoming_sub_title=&live_sub_title=&rec_sub_title=&upcoming_button_text=&live_button_text=&rec_button_text=&link_to_channel=false&form_type=all_fields&widget_width=&widget_height=0&enable_iframe=true&background_color=&btext_color=&link_color=0050ff&ltext_color=ffffff&redirect_to_confirmation_page=1&widget_button_registered_content=&widget_webinar_descriptions=&widget_redirect_type=redirect&cid=cec48b5ceb5c`;
+  const scriptSrc = `https://web.bigmarker.com/widget/register_widget.js?club=wipfli&conference=${conferenceId}&widget_type=form_register&series_register=&upcoming_sub_title=&live_sub_title=&rec_sub_title=&upcoming_button_text=&live_button_text=&rec_button_text=&link_to_channel=false&form_type=all_fields&widget_width=&widget_height=0&enable_iframe=false&background_color=&btext_color=&link_color=0050ff&ltext_color=ffffff&redirect_to_confirmation_page=1&widget_button_registered_content=&widget_webinar_descriptions=&widget_redirect_type=redirect&cid=cec48b5ceb5c`;
 
   return (
     <>
